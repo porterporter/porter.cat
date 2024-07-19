f8d7919e9ec080c8dd94dd70fb6b4061bf29ab5c
porter milton <57007161+porterporter@users.noreply.github.com>
2024-07-18 22:11:28 -0500

    rss for my webfriends!

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 import { build } from 'velite';
+import { Feed } from 'feed';
+import { readFileSync, writeFileSync } from 'fs';
 
 /** @type {import('next').NextConfig} */
 const config = {
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
+    config.plugins.push(new VeliteRssPlugin());
     return config;
   },
 };
@@ -20,6 +23,54 @@ class VeliteWebpackPlugin {
       VeliteWebpackPlugin.started = true;
       const dev = compiler.options.mode === 'development';
       await build({ watch: dev, clean: !dev });
+    });
+  }
+}
+
+class VeliteRssPlugin {
+  static started = false;
+  static rendered = 0;
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    compiler.hooks.afterEmit.tap('VeliteRssPlugin', async (compilation) => {
+      if (VeliteRssPlugin.started) return;
+      VeliteRssPlugin.rendered++;
+      VeliteRssPlugin.started = true;
+      console.log('help!');
+      const postFiles = readFileSync('./.velite/posts.json', 'utf-8');
+      const posts = JSON.parse(postFiles);
+
+      const feedOptions = {
+        title: `porter.cat`,
+        description:
+          "I'm Porter, a software engineer and designer. I write about web development, networking, and other things I find interesting.",
+        id: `porter.cat`,
+        link: 'https://porter.cat',
+        favicon: `https://porter.cat/favicon.ico`,
+        copyright: `© ${new Date().getFullYear()}, Porter`,
+        feedLinks: {
+          rss2: `https://porter.cat/rss.xml`,
+          json: `https://porter.cat/rss.json`,
+          atom: `https://porter.cat/atom.xml`,
+        },
+      };
+
+      const feed = new Feed(feedOptions);
+
+      posts.map((post) => {
+        feed.addItem({
+          title: post.title,
+          description: post.description,
+          link: `https://porter.cat/blog/${post.slug}`,
+          guid: post.slug,
+          date: new Date(post.date),
+        });
+      });
+
+      writeFileSync('./public/rss.xml', feed.rss2());
+      writeFileSync('./public/rss.json', feed.json1());
+      writeFileSync('./public/atom.xml', feed.atom1());
+
+      console.log(`RSS feeds generated. Pass: ${VeliteRssPlugin.rendered}`);
     });
   }
 }
