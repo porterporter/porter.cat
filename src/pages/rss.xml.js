018acf6ce11fffd330c3367625b5828859f3587d
porter milton <57007161+porterporter@users.noreply.github.com>
2025-04-13 19:48:34 -0500

    update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,9 +7,16 @@ export async function GET(context) {
 		title: 'porter',
 		description: 'my homepage, blog, and portfolio',
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: posts.map((post) => {
+			const body =
+				post.description ?? post.body.split('\n').slice(0, 5).join('\n');
+			body += `\n<a href="/writing/${post.id}/">[Read the rest here]</a>`;
+			return {
+				pubDate: post.data.published,
+				title: post.data.title ?? post.id,
+				description: post.data.description,
+				link: `/writing/${post.id}/`,
+			};
+		}),
 	});
 }
